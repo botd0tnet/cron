4c5d4e1cbafd63e06886af3a17878c3e2c9bad37
botd0tnet <74009258+botd0tnet@users.noreply.github.com>
2021-03-04 13:45:17 +0000

    Create cronjob.sh

---
+++ cronjob.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for(( i=1; i <=50; i++ ))
+do
+	mix='nym-mixnode'
+	nymmixnode=${mix}${i}
+	if [[ ! "$(/usr/sbin/service ${nymmixnode} status)" =~ "start/running" ]]
+	then
+		/usr/sbin/service ${nymmixnode} start
+		sleep 30
+	fi
+done
